bbf06b1924d16e42baac3a7da90b26aa35a94a2f
grudus <jakgruda@gmail.com>
2019-06-02 23:13:40 +0200

    Add http methods

--- src/app/books/book-service.service.ts
+++ src/app/books/book-service.service.ts
@@ -1,13 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Book } from './models/Book';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookServiceService {
 
-  private baseUrl = 'http://localhost:3000';
+  private baseUrl = 'http://localhost:3000/Service1.svc/json';
 
   constructor(private httpClient: HttpClient) {
   }
+
+
+  getAll(): Observable<Book[]> {
+    return this.httpClient.get<Book[]>(`${this.baseUrl}/books`);
+  }
+
+
 }
