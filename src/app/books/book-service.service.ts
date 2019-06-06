9008d20714007b85f535c954799440c998749c38
grudus <jakgruda@gmail.com>
2019-06-06 10:07:39 +0200

    Parse xml response to json

--- src/app/books/book-service.service.ts
+++ src/app/books/book-service.service.ts
@@ -2,35 +2,49 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Book } from './models/Book';
+import * as xml2js from '../utils/json2xml';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookServiceService {
 
-  private baseUrl = 'http://web80.website.net.ii.pwr.wroc.pl/rsi05/Service1.svc/json';
+  private baseUrl = 'http://web80.website.net.ii.pwr.wroc.pl/rsi05/Service1.svc';
 
   constructor(private httpClient: HttpClient) {
   }
 
 
   getAll(): Observable<Book[]> {
-    return this.httpClient.get<Book[]>(`${this.baseUrl}/books`);
+    return this.httpClient.get<Book[]>(`${this.baseUrl}/xml/books`, {responseType: 'text' as 'json'})
+      .pipe(
+        map(res => {
+          const json = xml2js.xmlToJson(xml2js.textToXml(res)) as { ArrayOfBook: { Book: Book[] } };
+          return json.ArrayOfBook.Book;
+        })
+      );
   }
 
   getById(id: string): Observable<Book> {
-    return this.httpClient.get<Book>(`${this.baseUrl}/books/${id}`);
+    return this.httpClient.get<Book>(`${this.baseUrl}/xml/books/${id}`, {responseType: 'text' as 'json'})
+      .pipe(
+        map(res => {
+          const json = xml2js.xmlToJson(xml2js.textToXml(res)) as { Book: Book };
+          return json.Book;
+        })
+      );
   }
 
   addBook(book: Book): Observable<any> {
-    return this.httpClient.post(`${this.baseUrl}/books`, book, {responseType: 'text'});
+    return this.httpClient.post(`${this.baseUrl}/json/books`, book, {responseType: 'text'});
   }
 
   deleteBook(id: string): Observable<any> {
-    return this.httpClient.delete(`${this.baseUrl}/books/${id}`, {responseType: 'text'});
+    return this.httpClient.delete(`${this.baseUrl}/json/books/${id}`, {responseType: 'text'});
   }
 
   update(id: string, updatedBook: Book): Observable<any> {
-    return this.httpClient.put<Book>(`${this.baseUrl}/books/${id}`, updatedBook, {responseType: 'text' as 'json'});
+    return this.httpClient.put<Book>(`${this.baseUrl}/json/books/${id}`, updatedBook, {responseType: 'text' as 'json'});
   }
 }
